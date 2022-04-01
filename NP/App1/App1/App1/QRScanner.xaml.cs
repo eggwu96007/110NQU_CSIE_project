a6fa25485e227fe9130a8a143b97d72eb8ae8aef
nqusw109new <s110814315@student.nqu.edu.tw>
2022-04-02 01:50:03 +0800

    QRcranner

---
+++ NP/App1/App1/App1/QRScanner.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class QRScanner : ContentPage
+    {
+        public QRScanner()
+        {
+            InitializeComponent();
+        }
+        
+        private void scanView_OnScanResult(ZXing.Result result)
+        {
+            /*OpenWebCommand = new Command(async () => await Browser.OpenAsync(result));
+            public ICommand OpenWebCommand { get; }*/
+        /*Device.BeginInvokeOnMainThread(async () =>
+            {
+                await DisplayAlert("掃描結果", result.Text, "yes");
+
+               await Browser.OpenAsync("https://aka.ms/xamarin-quickstart")
+            });*/
+
+        Device.BeginInvokeOnMainThread(async () =>
+            {
+                await DisplayAlert("掃描結果", result.Text, "yes");
+                await Navigation.PushAsync(new menu());
+                //await Browser.OpenAsync(result.ToString());
+            });
+
+
+    }
+
+}
+}
